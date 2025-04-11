85d22dfc30296922b1021e7c69b026c0c7e0947c
killervaps <113247840+killervaps@users.noreply.github.com>
2025-04-11 13:49:38 +0700

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ from main import app
 client = TestClient(app)
 
 def test_health_endpoint():
-    response = client.get("/health")
+    response = client.get("/health-check")
     assert response.status_code == 200
     data = response.json()
     assert data["status"] == "UP"
